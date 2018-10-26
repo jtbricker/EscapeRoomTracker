4acc33fd5967bda3ff3f7e470dd4212f5a800c11
Justin Bricker <jt.bricker@gmail.com>
2018-10-26 14:10:23 -0400

    Use Sql DB with EntityFramework
    * Also added Edit links to Home and Details Pages

---
+++ EscapeRoomTracker/Migrations/20181026180016_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EscapeRoomTracker.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EscapeRooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    City = table.Column<string>(nullable: false),
+                    Escaped = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EscapeRooms", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EscapeRooms");
+        }
+    }
+}
--- EscapeRoomTracker/Startup.cs
+++ EscapeRoomTracker/Startup.cs
@@ -2,23 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EscapeRoomTracker.Data;
 using EscapeRoomTracker.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EscapeRoomTracker
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
-            services.AddSingleton<IEscapeRoomData, InMemoryEscapeRoomData>();
+            services.AddDbContext<EscapeRoomTrackerDbContext>(
+                options => options.UseSqlServer(_config.GetConnectionString("EscapeRoomTracker")));
+            services.AddScoped<IEscapeRoomData, SqlEscapeRoomData>();
             services.AddMvc();
         }
 
